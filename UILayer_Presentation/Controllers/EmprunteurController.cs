85e092980658a5e349ac505e8fb97e242fd1df36
LarissaF <larissafossi@gmail.com>
2023-09-08 18:57:05 +0200

    projet initial

---
+++ UILayer_Presentation/Controllers/EmprunteurController.cs
@@ -0,0 +1,266 @@
+﻿using Bll_Service.Service;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UILayer_Presentation.Controllers
+{
+    public class EmprunteurController : Controller
+    {
+        // IoC
+        private readonly IEmprunteurService es;
+
+        public EmprunteurController(IEmprunteurService ess)
+        {
+            this.es = ess;
+        }
+
+
+
+        // 1- Afficher la liste
+        public ActionResult ListeEmrpunteur()
+        {
+            List<Emprunteur> listeEmprunteurs =  es.GetAllEmprunteurs();
+            
+            return View(listeEmprunteurs); // On veut passer cette liste à la vue, pourqu'elle soit affichée à la vue
+                                           // Passer le model MVC (la liste des etudiants) à la vue en utilisant le mecanisme des vues fortement typées
+        }
+
+
+
+
+        // 2- Ajouter Emprunteur
+        // La methode d'action pour afficher le formulaire d'ajout
+        [HttpGet]
+        public ActionResult AjoutEmprunteur()
+        {
+            return View();
+        }
+
+        // La methode pour traiter le formulaire d'ajout
+        [HttpPost]
+        public ActionResult AjoutEmprunteur(Emprunteur emp)
+        {
+            // Valider les champs
+            if (string.IsNullOrEmpty(emp.Nom)) // IsNullOrEmpty est une methode static d'après la doc officielle, et conc faut l'appeller par sa classe (qui est string)
+            {
+                // Ajouter un message d'erreur dans l'objet ModelState en specifiant la propriété concernée du Model (personne) (Accepte 2 proppriétés en entrée)
+                ModelState.AddModelError("Nom", "Le champs du nom est obligatoire");
+            }
+
+            if (string.IsNullOrEmpty(emp.Prenom)) // IsNullOrEmpty est une methode static d'après la doc officielle, et conc faut l'appeller par sa classe (qui est string)
+            {
+                // Ajouter un message d'erreur dans l'objet ModelState en specifiant la propriété concernée du Model (emprunteur) (Accepte 2 proppriétés en entrée)
+                ModelState.AddModelError("Prenom", "Le champs du prenom est obligatoire");
+            }
+
+
+            if (string.IsNullOrEmpty(Convert.ToString(emp.AnneeNaissance)) | emp.AnneeNaissance < 1000 ) 
+            { // (string.IsNullOrEmpty(Convert.ToString(emp.AnneeNaissance)) || Convert.ToString(emp.AnneeNaissance).Length < 4 || Convert.ToString(emp.AnneeNaissance).Length > 4) // 
+
+                ModelState.AddModelError("AnnéeNaissance", "L'année de naissance est obligatoire");
+            }
+
+            // Verifier l'état du model (si les données aisies sont valides ou pas)
+            if (ModelState.IsValid)
+            {
+                int verif = es.AjoutEmprunteur(emp);
+                if (verif != 0)
+                {
+                    // Si l'ajout est bien passé, on fait une redirection vers la methode d'action ListeEmrpunteur pour afficher la nouvelle liste des emprunteurs
+                    return RedirectToAction("ListeEmrpunteur");
+                }
+                else
+                {
+                    ViewBag.Msg = "L'ajout est KO!";
+                    return View(); // Et reste sur la vue
+
+                }
+            }
+            
+            return View(); // Et reste sur la vue
+
+
+        }
+
+
+
+
+        // 3. La fonctionnalité rechercher par son Id
+        // Methode d'action pour afficher le formulaire de recherche
+        public ActionResult FindEmprunteur()
+        {
+            return View();
+        }
+
+
+        // Une methode dd'action pour traiter le formulaire de recherche
+        [HttpPost]
+        public ActionResult FindEmprunteur(int id) // Attention: Dans ce parametre, faut donner le même nom que le parametre de la requete id (name de la balise). Sinon il ne va pas le reconnaitre
+        {
+            // Appel de la méthode service pour rechercher l'emprunteur par son id
+            Emprunteur efind = es.GetEmprunteurById(id);
+            if (efind != null)
+            {
+                return View(efind);// IL retourne la vue avec eFind dans la table
+            }
+            else
+            {
+                ViewBag.Msg = "L'emprunteur n'existe pas";
+                return View();
+            }
+
+        }
+
+
+
+    /*    // 4. Fonctionnalité supprimer
+        // Une methode action pour afficher le formulaire de suppression
+        public ActionResult SuppEmprunteur()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SuppEmprunteur(int id) // Attention: Dans ce parametre, faut donner le même nom que le parametre de la requete id (name de la balise). Sinon il ne va pas le reconnaitre
+        {
+            int verif = es.SuppEmprunteur(id);
+
+            if (verif != 0)
+            {
+                // Redirection vers l'action Index afin d'afficher la nouvelle liste
+                return RedirectToAction("ListeEmrpunteur");
+            }
+            else
+            {
+                ViewBag.Msg = "La suppression a échouée !!!";
+                return View();
+            }
+
+        } */
+
+
+
+
+        // 4 Fonctionnalité Modifier
+        // Methode d'action pour afficher le formulaire de modification
+
+        public ActionResult ModifEmprunteur()
+        {
+            return View();
+        }
+
+
+       // Methode d'action pour traiter le formulaire de modification
+        [HttpPost]
+        public ActionResult ModifEmprunteur(Emprunteur emp)
+        {
+            int verif = es.ModifEmprunteur(emp);
+            if (verif != 0)
+            {
+                // Redirection vers l'action Index afin d'afficher la nouvelle liste
+                return RedirectToAction("ListeEmrpunteur");
+            }
+            else
+            {
+                ViewBag.Msg = "La modification a échouée !!!";
+                return View();
+            }
+
+        }
+
+
+
+        // Operateur par lien
+        // En cliquant sur "modifier" sur la ligne de l'emprunteur concerné daans la liste, ça va faire appel à cette méthode d'action.
+        // Cette méthode va trouver l'emprunteur à travers le id recuperer, et l'envoyer au à la methode d'action get de modifEmprunteur, pour afficher le formulaire (sans param).
+        // Lors de la validation du formulaire, ca va faire appel à la méthode post de modifEmprunteur
+        public ActionResult ModifEmpLink(int id)
+        {
+            // Chercher toutes les infos de l'étudiant et les passer à la vue du model
+            var eFind = es.GetEmprunteurById(id);
+
+            // Faire une redirection vers l'action Update de ce controller etudiant pour qu'il affiche les infos de l'etudiant sur la vue update. Et là on peut faire les modifications qu'on souhaite.
+            return View("ModifEmprunteur", eFind);
+        }
+
+
+
+
+        // 5. Fonctionnalité supprimer
+        // Operateur par lien: Les operateurs de lien sont des get
+        // Ceci n'a pas besoin de post.
+
+        // En cliquant sur "supprimer" sur la ligne de l'emprunteur concerné dans la liste, ça va faire appel à cette méthode d'action.
+        // Cette méthode va supprimer directement l'emprunteur, et actualiser la liste en retournant l'action sur Liste d'emprunteur, qui va actualiser la liste via GetAllEmp
+
+
+        [HttpGet]
+        public ActionResult SuppEmpLink(int id)
+        {
+            // appeler la methode supprimer de service
+            int verif = es.SuppEmprunteur(id);
+
+            // Faire une redirection vers l'action index
+            return RedirectToAction("ListeEmrpunteur");
+        }
+
+
+
+
+
+        // 6. Liste des livres d'un emprunteur
+        public ActionResult FindLivreEmp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ListeLivreEmp(int id)
+        {
+            List<Livre> listeLivresEmp = es.GetAllLivreEmprunteurId(id);
+
+            return View(listeLivresEmp);
+        }
+
+        public ActionResult ListeLivreEmp()
+        {
+            var emp = (Emprunteur)Session["emprunteurSession1"];
+            List<Livre> listeLivresEmp = es.GetAllLivreEmprunteurId(emp.Id);
+
+            return View(listeLivresEmp);
+        }
+
+
+
+        // 6. Ajouter un livre
+        // Operateur par lien
+        // En cliquant sur "AjoutLivre" sur la ligne de l'emprunteur concerné daans la liste, ça va faire appel à cette méthode d'action.
+        // Cette méthode va trouver l'emprunteur à travers le id recuperer, et l'envoyer au à la methode d'action get de modifEmprunteur, pour afficher le formulaire (sans param).
+        // Lors de la validation du formulaire, ca va faire appel à la méthode post de modifEmprunteur
+        public ActionResult AjoutLivreLink(int id)
+        {
+            // Chercher toutes les infos de l'étudiant et les passer à la vue du model
+            var eFind = es.GetEmprunteurById(id);
+            Session["emprunteurSession1"] = eFind;
+
+            // Faire une redirection vers l'action AjoutLivre du controller Livre pour qu'il affiche le formulaire d'ajout d'un livre.
+            return RedirectToAction("AjoutLivreEmprunteur", "Livre");
+        }
+
+
+
+        // Supprimer un livre d'un emprunteur
+
+        public ActionResult SuppLivreLink(int id)
+        {
+            var eFind = es.GetEmprunteurById(id);
+            Session["emprunteurSession2"] = eFind;
+
+            return RedirectToAction("ListeLivresEmprunteur", "Livre"); // Il va afficher la liste des livres, et là on pourra supprimer le livre qu'on souhaite
+        }
+    }
+}
---
+++ UILayer_Presentation/Controllers/LivreController.cs
@@ -0,0 +1,336 @@
+﻿using Bll_Service.Service;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UILayer_Presentation.Controllers
+{
+    public class LivreController : Controller
+    {
+        // IoC
+        private readonly ILivreService ls;
+
+        public LivreController(ILivreService lss)
+        {
+            this.ls = lss;
+        }
+
+
+
+        // 1- Liste livre
+        public ActionResult ListeLivre()
+        {
+            List<Livre> listeLivres = ls.GetAllLivres();
+            return View(listeLivres);
+        }
+
+
+
+        // 2- Ajout Livre
+        [HttpGet]
+        public ActionResult AjoutLivre()
+        {
+            return View();
+        }
+
+
+        // La methode pour traiter le formulaire d'ajout
+        [HttpPost]
+        public ActionResult AjoutLivre(Livre liv)
+        {
+            // Valider les champs
+            if (string.IsNullOrEmpty(liv.Auteur)) // IsNullOrEmpty est une methode static d'après la doc officielle, et conc faut l'appeller par sa classe (qui est string)
+            {
+                // Ajouter un message d'erreur dans l'objet ModelState en specifiant la propriété concernée du Model (personne) (Accepte 2 proppriétés en entrée)
+                ModelState.AddModelError("Auteur", "Le champs de l'auteur est obligatoire");
+            }
+
+            if (string.IsNullOrEmpty(liv.Titre)) // IsNullOrEmpty est une methode static d'après la doc officielle, et conc faut l'appeller par sa classe (qui est string)
+            {
+                // Ajouter un message d'erreur dans l'objet ModelState en specifiant la propriété concernée du Model (livre) (Accepte 2 proppriétés en entrée)
+                ModelState.AddModelError("Titre", "Le champs du titre est obligatoire");
+            }
+
+            // Valider le champ à la propriété Age de Personne
+            if (string.IsNullOrEmpty(Convert.ToString(liv.Annee)) || liv.Annee < 1000 ) 
+            { // string.IsNullOrEmpty(Convert.ToString(liv.Annee)) || Convert.ToString(liv.Annee).Length < 4 || Convert.ToString(liv.Annee).Length > 4) // 
+
+                ModelState.AddModelError("Année", "L'année est obligatoire");
+            }
+
+            // Verifier l'état du model (si les données aisies sont valides ou pas)
+            if (ModelState.IsValid)
+            {
+                int verif = ls.AjoutLivre(liv);
+                if (verif != 0)
+                {
+                    // Si l'ajout est bien passé, on fait une redirection vers la methode d'action ListeEmrpunteur pour afficher la nouvelle liste des emprunteurs
+                    return RedirectToAction("ListeLivre");
+                }
+                else
+                {
+                    ViewBag.Msg = "L'ajout est KO!";
+                    return View(); // Et reste sur la vue
+
+                }
+            }
+
+            return View(); // Et reste sur la vue
+
+
+        }
+
+
+
+
+        // 3. La fonctionnalité rechercher livre par son Id
+        // Methode d'action pour afficher le formulaire de recherche
+        public ActionResult FindLivre()
+        {
+            return View();
+        }
+
+
+        // Une methode dd'action pour traiter le formulaire de recherche
+        [HttpPost]
+        public ActionResult FindLivre(int id) // Attention: Dans ce parametre, faut donner le même nom que le parametre de la requete id (name de la balise). Sinon il ne va pas le reconnaitre
+        {
+            // Appel de la méthode service pour rechercher le livre par son id
+            Livre efind = ls.GetLivresById(id);
+            if (efind != null)
+            {
+                return View(efind);// IL retourne la vue avec eFind dans la table
+            }
+            else
+            {
+                ViewBag.Msg = "Le livre n'existe pas";
+                return View();
+            }
+
+        }
+
+
+
+
+
+
+
+        // 4 Fonctionnalité Modifier
+        // Methode d'action pour afficher le formulaire de modification
+
+        public ActionResult ModifLivre()
+        {
+            return View();
+        }
+
+
+        // Methode d'action pour traiter le formulaire de modification
+        [HttpPost]
+        public ActionResult ModifLivre(Livre liv)
+        {
+            int verif = ls.ModifLivre(liv);
+            if (verif != 0)
+            {
+                // Redirection vers l'action Index afin d'afficher la nouvelle liste
+                return RedirectToAction("ListeLivre");
+            }
+            else
+            {
+                ViewBag.Msg = "La modification a échouée !!!";
+                return View();
+            }
+
+        }
+
+
+        public ActionResult ModifLivreEmprunteur()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ModifLivreEmprunteur(Livre liv)
+        {
+            int verif = ls.ModifLivre(liv);
+            if (verif != 0)
+            {
+                // Redirection vers l'action Index afin d'afficher la nouvelle liste
+                return RedirectToAction("ListeLivresEmprunteur");
+            }
+            else
+            {
+                ViewBag.Msg = "La modification a échouée !!!";
+                return View();
+            }
+
+        }
+
+
+
+        // Operateur par lien
+        // En cliquant sur "modifier" sur la ligne du livre concerné dans la liste, ça va faire appel à cette méthode d'action.
+        // Cette méthode va trouver le livre à travers le id recuperé, et l'envoyer à la methode d'action get de modifLivre, pour afficher le formulaire (sans param).
+        // Lors de la validation du formulaire, ca va faire appel à la méthode post de modifLivre
+        public ActionResult ModifLivLink(int id)
+        {
+            // Chercher toutes les infos du livre et les passer à la vue du model
+            var eFind = ls.GetLivresById(id);
+
+            // Faire une redirection vers l'action Update de ce controller etudiant pour qu'il affiche les infos du livre sur la vue update. Et là on peut faire les modifications qu'on souhaite.
+            return View("ModifLivre", eFind);
+        }
+
+
+        public ActionResult ModifLivLink2(int id)
+        {
+            // Chercher toutes les infos du livre et les passer à la vue du model
+            var eFind = ls.GetLivresById(id);
+
+            // Faire une redirection vers l'action Update de ce controller etudiant pour qu'il affiche les infos du livre sur la vue update. Et là on peut faire les modifications qu'on souhaite.
+            return View("ModifLivreEmprunteur", eFind);
+        }
+
+
+
+
+
+        // 5. Fonctionnalité supprimer
+        // Operateur par lien: Les operateurs de lien sont des get
+        // Ceci n'a pas besoin de post.
+
+        // En cliquant sur "supprimer" sur la ligne du livre concerné dans la liste, ça va faire appel à cette méthode d'action.
+        // Cette méthode va supprimer directement le livre, et actualiser la liste en retournant l'action sur Liste des livres, qui va actualiser la liste via GetAllLivres
+
+
+        [HttpGet]
+        public ActionResult SuppLivLink(int id)
+        {
+            // appeler la methode supprimer de service
+            int verif = ls.SuppLivre(id);
+
+            // Faire une redirection vers l'action index
+            return RedirectToAction("ListeLivre");
+        }
+
+
+        [HttpGet]
+        public ActionResult SuppLivLink2(int id)
+        {
+            // appeler la methode supprimer de service
+            int verif = ls.SuppLivre(id);
+
+            // Faire une redirection vers l'action index
+            return RedirectToAction("ListeLivresEmprunteur");
+        }
+
+
+
+
+        // 3. La fonctionnalité rechercher livre par IdEmprunteur
+        // Methode d'action pour afficher le formulaire de recherche
+        public ActionResult FindLivresEmprunteur()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ListeLivresEmprunteur(int id) // Attention: Dans ce parametre, faut donner le même nom que le parametre de la requete id (name de la balise). Sinon il ne va pas le reconnaitre
+        {
+            // Appel de la méthode service pour rechercher le livre par son id
+            List<Livre> listeLivre = ls.GetLivresByIdEmprunteur(id);
+            if (listeLivre != null)
+            {
+                return View(listeLivre);// IL retourne la vue avec listeLivre dans la table
+            }
+            else
+            {
+                ViewBag.Msg = "L'Id de l'emprunteur n'existe pas";
+                return View();
+            }
+
+        }
+
+        public ActionResult ListeLivresEmprunteur() // Attention: Dans ce parametre, faut donner le même nom que le parametre de la requete id (name de la balise). Sinon il ne va pas le reconnaitre
+        {
+            Emprunteur emp = (Emprunteur)Session["emprunteurSession2"];
+
+            // Appel de la méthode service pour rechercher le livre par son id
+            List<Livre> listeLivre = ls.GetLivresByIdEmprunteur(emp.Id);
+            if (listeLivre != null)
+            {
+                return View(listeLivre);// IL retourne la vue avec listeLivre dans la table
+            }
+            else
+            {
+                ViewBag.Msg = "L'Id de l'emprunteur n'existe pas";
+                return View();
+            }
+
+        }
+
+
+
+
+
+        [HttpGet]
+        public ActionResult AjoutLivreEmprunteur()
+        {
+            var emp = (Emprunteur)Session["emprunteurSession1"];
+            Livre liv = new Livre();
+            liv.Id = emp.Id;
+            return View(liv);
+        }
+
+
+        // La methode pour traiter le formulaire d'ajout
+        [HttpPost]
+        public ActionResult AjoutLivreEmprunteur(Livre liv)
+        {
+            // Valider les champs
+            if (string.IsNullOrEmpty(liv.Auteur)) // IsNullOrEmpty est une methode static d'après la doc officielle, et conc faut l'appeller par sa classe (qui est string)
+            {
+                // Ajouter un message d'erreur dans l'objet ModelState en specifiant la propriété concernée du Model (personne) (Accepte 2 proppriétés en entrée)
+                ModelState.AddModelError("Auteur", "Le champs de l'auteur est obligatoire");
+            }
+
+            if (string.IsNullOrEmpty(liv.Titre)) // IsNullOrEmpty est une methode static d'après la doc officielle, et conc faut l'appeller par sa classe (qui est string)
+            {
+                // Ajouter un message d'erreur dans l'objet ModelState en specifiant la propriété concernée du Model (personne) (Accepte 2 proppriétés en entrée)
+                ModelState.AddModelError("Titre", "Le champs du titre est obligatoire");
+            }
+
+            // Valider le champ à la propriété Age de Personne
+            if (string.IsNullOrEmpty(Convert.ToString(liv.Annee)) || Convert.ToString(liv.Annee).Length < 4 || Convert.ToString(liv.Annee).Length > 4) // 
+            {
+                ModelState.AddModelError("Année", "L'année est obligatoire");
+            }
+
+            // Verifier l'état du model (si les données aisies sont valides ou pas)
+            if (ModelState.IsValid)
+            {
+                int verif = ls.AjoutLivre(liv);
+                if (verif != 0)
+                {
+                    
+                    // Si l'ajout est bien passé, on fait une redirection vers la methode d'action AjoutEmrpunteur pour afficher la nouvelle liste des emprunteurs
+                    return RedirectToAction("ListeLivreEmp", "Emprunteur");
+                }
+                else
+                {
+                    ViewBag.Msg = "L'ajout est KO!";
+                    return View(); // Et reste sur la vue
+
+                }
+            }
+
+            return View(); // Et reste sur la vue
+
+
+        }
+
+    }
+}
